d9ce02beb0b830a9d15eb94b69ce8bac9fed1641
jasa89 <78540352+jasa89@users.noreply.github.com>
2025-12-09 13:17:31 +0200

    done

--- app/src/main/java/com/example/scaffoldandnavigation/MainActivity.kt
+++ app/src/main/java/com/example/scaffoldandnavigation/MainActivity.kt
@@ -4,17 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.*
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
+import com.example.scaffoldandnavigation.ui.screens.ScaffoldApp
 import com.example.scaffoldandnavigation.ui.theme.ScaffoldAndNavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,53 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ScaffoldAndNavigationTheme {
-                Scaffold(
-                    topBar = {
-                        var expanded by remember { mutableStateOf(false) }
-                        TopAppBar(
-                            title = { Text("Top app bar") },
-                            colors = TopAppBarDefaults.topAppBarColors(
-                                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                titleContentColor = MaterialTheme.colorScheme.primary,
-                            ),
-                            navigationIcon = {
-                                IconButton(onClick = { /* TODO */ }) {
-                                    Icon(
-                                        imageVector = Icons.Filled.Menu,
-                                        contentDescription = "Menu Icon"
-                                    )
-                                }
-                            },
-                            actions = {
-                                IconButton(onClick = { expanded = !expanded }) {
-                                    Icon(
-                                        imageVector = Icons.Filled.MoreVert,
-                                        contentDescription = "Open menu"
-                                    )
-                                }
-                                DropdownMenu(
-                                    expanded = expanded,
-                                    onDismissRequest = { expanded = false }
-                                ) {
-                                    DropdownMenuItem(
-                                        text = { Text("Settings") },
-                                        onClick = {}
-                                    )
-                                    DropdownMenuItem(
-                                        text = { Text("Info") },
-                                        onClick = {}
-                                    )
-                                }
-                            }
-                        )
-                    },
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    Text(
-                        text = "Hello Android!",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ScaffoldApp()
             }
         }
     }
